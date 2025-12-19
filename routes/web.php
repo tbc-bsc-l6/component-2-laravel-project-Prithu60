c72bb58e4c16f2e413b74982ab1798294e388f5b
Prithu60 <aprithu23@tbc.edu.np>
2025-12-19 16:01:20 +0545

    Add admin module creation functionality

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\StudentEnrollmentController;
+
+Route::post('/modules/{module}/enroll', [StudentEnrollmentController::class, 'enroll'])
+    ->middleware(['auth'])
+    ->name('modules.enroll');
+
+use App\Http\Controllers\Admin\ModuleController as AdminModuleController;
+
+Route::post('/admin/modules', [AdminModuleController::class, 'store'])
+    ->middleware(['auth'])
+    ->name('admin.modules.store');
